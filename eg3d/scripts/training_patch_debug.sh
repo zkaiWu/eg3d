59ca1dd799b729cb1dc00b37a7d934a19f5d8a48
zkaiWu <sy2221114@buaa.edu.cn>
2023-07-13 14:20:34 +0800

    patch_sr debug

--- eg3d/scripts/training_patch_debug.sh
+++ eg3d/scripts/training_patch_debug.sh
@@ -3,9 +3,10 @@
 CIDX=$1
 GPU_NUM=$2
 BATCH_SIZE=$3
+OUTPUT_DIR=$4
 
 CUDA_VISIBLE_DEVICES=$CIDX python train.py \
-    --outdir=exp/training_patch_debug \
+    --outdir=$OUTPUT_DIR \
     --cfg=ffhq \
     --data=/data5/wuzhongkai/data/dreamfusion_data/eg3d_generation_data_256floyd_ffhqformat \
     --gpus=$GPU_NUM \
