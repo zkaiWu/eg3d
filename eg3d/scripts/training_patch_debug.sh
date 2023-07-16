45e66d4ca871767c9087dc921e592275d44d1228
zkaiWu <sy2221114@buaa.edu.cn>
2023-07-16 13:41:10 +0800

    submit for running

--- eg3d/scripts/training_patch_debug.sh
+++ eg3d/scripts/training_patch_debug.sh
@@ -8,10 +8,9 @@ OUTPUT_DIR=$4
 CUDA_VISIBLE_DEVICES=$CIDX python train.py \
     --outdir=$OUTPUT_DIR \
     --cfg=ffhq \
-    --data=/home2/zhongkaiwu/data/dreamfusion_data/eg3d_fake/eg3d_generation_data_g4.0_noise250_promptlist \
+    --data=/home/zhongkaiwu/data/dreamfusion_data/eg3d_fake/eg3d_generation_data_256floyd_ffhqformat \
     --gpus=$GPU_NUM \
     --batch=$BATCH_SIZE \
     --gamma=1 \
     --gen_pose_cond=True \
     --metrics fid2k_full_for_patch_rendering \
-    --mbstd-group 2 \
