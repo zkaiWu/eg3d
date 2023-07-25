5285bed711cfdb7103dbafffca27291e671f0215
wzkwzk1998 <869693441@qq.com>
2023-07-25 21:17:18 +0800

    add file for blender 360 sampling and generate camera_path file'

--- dataset_preprocessing/blender_process/nerfstudio_blender_sampling.py
+++ dataset_preprocessing/blender_process/nerfstudio_blender_sampling.py
@@ -24,20 +24,21 @@ def normalize_torch(x: torch.Tensor, dim: int=-1) -> torch.Tensor:
     """
     return x / (torch.norm(x, dim=dim, keepdim=True))
 
-def normalize_np(x: np.ndarray, axis: int=-1) -> np.ndarray:
+def normalize_np(x: np.ndarray, axis: int=-1):
     return x / (np.linalg.norm(x, axis=axis, keepdims=True))
 
 def calc_camera_matrix(pitch, yaw, radius):
 
     camera_origin = np.array([
-        radius * np.sin(pitch) * np.cos(yaw),
-        radius * np.cos(pitch), 
-        radius * np.sin(pitch) * np.sin(yaw),
+        radius * np.sin(yaw) * np.cos(pitch),
+        radius * np.sin(yaw) * np.sin(pitch),
+        radius * np.cos(yaw), 
     ])
-    camera_origin = normalize_np(camera_origin)
+    print(f"z: {camera_origin[2]}")
 
-    forward_vector = -camera_origin
-    up_vector = np.array([0, 1, 0])
+    forward_vector = -camera_origin 
+    forward_vector = normalize_np(forward_vector)
+    up_vector = np.array([0, 0, 1])
 
     right_vector = np.cross(forward_vector, up_vector)
 
@@ -48,36 +49,70 @@ def calc_camera_matrix(pitch, yaw, radius):
     camera_metrix = np.eye(4)
     camera_metrix[:3, :3] = np.stack([right_vector, up_vector, -forward_vector], axis=-1)
     camera_metrix[:3, 3] = camera_origin
+    print(camera_metrix)
     
     return camera_metrix
 
 
 def sample_pose_camera_path(args):
     camera_path = {}
-    camera_path["seconds"] = 2             # fake seconds
+    camera_path["seconds"] = 4             # fake seconds
     camera_path["camera_type"] = 'perspective'
     camera_path["render_height"] = camera_path["render_width"] = args.resolution
     camera_path["camera_path"] = []
+    camera_path['keyframes'] = []
+    camera_path['fps'] = 24
+    camera_path['is_cycle'] = False
+    camera_path['crop'] = None
+    camera_path['smoothness_value'] = 0.5
+
     
     for i in range(args.sample_num):
         # pitch = np.random.uniform(args.yaw_range[0], args.yaw_range[1])
         # yaw = np.random.uniform(args.pitch_range[0], args.pitch_range[1])
         gamma_p = np.random.uniform(0, 2*np.pi)
-        gamma_y = np.random.uniform(0, 0.5)
+        gamma_y = np.random.uniform(0, 0.)
         pitch = gamma_p
         yaw = np.arccos(1 - 2 * gamma_y)
         radius = np.random.uniform(args.radius_range[0], args.radius_range[1])
+        # print(pitch)
+        # print(yaw)
+        # print(radius)
 
-        metrix = calc_camera_matrix(pitch, yaw, radius).reshape(16)
+        metrix = calc_camera_matrix(pitch, yaw, radius)
         meta_data = {
-            'camera_to_world': metrix.tolist(),
+            'camera_to_world': metrix.reshape(16).tolist(),
             'fov': 50,
             'aspect': 1
         }
         camera_path["camera_path"].append(meta_data)
+        matrix_str = custom_np_2_str(np.transpose(metrix).reshape(16))
+        keyframe_data = {
+            # 'matrix':  np.array2string(np.transpose(metrix).reshape(16), separator=',', prefix='[', suffix=']'),
+            'matrix': matrix_str,
+            'fov': 50,
+            'aspect': 1,
+            'properties': f'[["FOV",50],["NAME","Camera {i}"],["TIME",{i}]]',
+
+        }
+        camera_path['keyframes'].append(keyframe_data)
     
     with open(args.output_dir, 'w') as json_file:
         json.dump(camera_path, json_file, indent=4)
+
+
+
+def custom_np_2_str(matrix: np.array):
+    str = '['
+    for i in matrix[:-1]:
+        if i == 0:
+            str += '0,'
+        else:
+            str += '{:.12f},'.format(i)
+    str += '{:.12f}'.format(matrix[-1]) if matrix[-1] != 0 else '0'
+    str += ']'
+
+    return str
 
 
 if __name__ == "__main__":
